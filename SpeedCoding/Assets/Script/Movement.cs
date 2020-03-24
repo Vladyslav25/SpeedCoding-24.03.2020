b98b875da341dd128e82420ec225a5493048d4ba
Vladi <vladi.elester@gmail.com>
2020-03-24 15:19:25 +0100

    Delete old, Add ScriptabelObjects
    Delete none used old Scripts
    Add ScriptabelObjects: PlayerStats & PlaySettings
    Edit Script: Movement

---
+++ SpeedCoding/Assets/Script/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    public PlayerStats m_playerStats;
+    public PlayerSettings m_playerSettings;
+
+    private Rigidbody m_playerRig;
+
+    private void Start()
+    {
+        m_playerRig = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Update()
+    {
+        Rotation();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_playerRig.AddForce(transform.forward * m_playerSettings.MovmentSpeed);
+        }
+    }
+
+    private void Rotation()
+    {
+        float X = Input.GetAxis("Mouse X") * m_playerSettings.RotationSpeed_X;
+        float Y = Input.GetAxis("Mouse Y") * m_playerSettings.RotationSpeed_Y;
+
+        transform.Rotate(0, X, 0);
+
+        if(!(Camera.main.transform.eulerAngles.x + (-Y) > m_playerSettings.Cam_Y_Min_Degree && 
+            Camera.main.transform.eulerAngles.x + (-Y) < m_playerSettings.Cam_Y_Max_Degree))
+        {
+            Camera.main.transform.RotateAround(transform.position, Camera.main.transform.right, -Y);
+        }
+    }
+}
---
+++ SpeedCoding/Assets/Script/PlayerStats.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerStats", menuName = "Player/Stats", order = 1)]
+public class PlayerStats : ScriptableObject
+{
+    [HideInInspector]
+    public bool isAlive = true;
+    [HideInInspector]
+    public bool haveStamina = true;
+
+    [SerializeField]
+    private float m_maxHp;
+    [SerializeField]
+    private float m_hp;
+    [SerializeField]
+    private float m_maxDmg = float.MaxValue;
+
+    [SerializeField]
+    private float m_maxStamina;
+    [SerializeField]
+    private float m_stamina;
+
+
+    public float HP
+    {
+        get
+        {
+            return m_hp;
+        }
+        set
+        {
+            m_hp = DamageTaken(value);
+        }
+    }
+
+    public float Stamina
+    {
+        get
+        {
+            return m_stamina;
+        }
+        set
+        {
+            m_stamina = AddStamina(value);
+        }
+    }
+
+    private float AddStamina(float value)
+    {
+        float newStamina = m_stamina + value;
+        if (newStamina >= m_maxStamina)
+        {
+            m_stamina = m_maxStamina;
+            return m_maxStamina;
+        }
+        else if (newStamina < 0)
+        {
+            m_stamina = 0;
+            haveStamina = false;
+            return m_stamina;
+        }
+        if (!haveStamina)
+            haveStamina = true;
+        return newStamina;
+    }
+
+    private float DamageTaken(float value)
+    {
+        if (value <= m_maxDmg)
+        {
+            float newHP = m_hp - value;
+            if (newHP < 0)
+            {
+                isAlive = false;
+                return 0;
+            }
+            return newHP;
+        }
+        throw new Exception("Es wurde mehr Schaden gemchat als erlaubt!");
+    }
+}
